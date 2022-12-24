6a43edea4704f104ce596652c8f90be04291b829
Max <67369623+scalette@users.noreply.github.com>
2022-12-24 11:10:55 +0300

    feat: json handling and base events for shop

--- src/app/views/home.ts
+++ src/app/views/home.ts
@@ -1,9 +1,17 @@
 import { Component } from "../../frameworks/root/component";
-import { EventsManager, EventTypes, IConfigComponent } from "../../types";
+import {
+  EventsManager,
+  EventTypes,
+  IConfigComponent,
+  ComponentData,
+} from "../../types";
 
 class Home extends Component {
   constructor(config: IConfigComponent) {
     super(config);
+    this.data = {
+      nameCard: "CARD#1",
+    };
   }
   public events(): EventsManager {
     console.log("proper events");
@@ -32,7 +40,7 @@ export const home: Home = new Home({
   <div class="container">
   <div class="col">
           <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">{{nameCard}}</text></svg>
 
             <div class="card-body">
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
